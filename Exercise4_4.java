ccb472a4a98d03c93df09e6b71b5929ae79c26e3
cansudesayin <250541096@firat.edu.tr>
2025-12-03 22:29:39 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -12,9 +12,12 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        //1-cevap: getNumber() çağrılır ama değeri kullanılmaz → Java hata vermez, uyarı da vermez. Return edilen değer yok sayılır.
+        
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // 2-cevap: void metot bir ifadenin içinde kullanılamaz. Java “void value cannot be used” veya “bad operand types for +” şeklinde hata üretir. 
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
